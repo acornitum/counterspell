e3c1bb03fe5039e5ccd752debe49d0ab160c9286
acon <101356372+acornitum@users.noreply.github.com>
2024-09-04 16:56:07 -0400

    [landing] added font + descriptor text

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 function App() {
   return (
     <div>
-      <div class="landing" className="flex flex-col justify-center h-screen 100vh">
+      <div class="landing" className="flex flex-col justify-center h-screen">
         <div className="text-center">
-          <p className="text-6xl">Counterspell</p>
-          <p className="text-xl">Join 5000+ students in any of 250+ locations to create a game</p>
+          <p className="text-8xl mb-4">Counterspell</p>
+          <p className="text-3xl serif mb-4">Join 5000+ students in any of 250+ locations to create games</p>
+
+          <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center bg-red-500 w-64 h-10">
+              <p className="serif">enter email here</p>
+            </div>
+          </div>
+          <p className="serif">November 9-10th, 2024 // in-person, worldwide</p>
+          
         </div>
+        
       </div>
 
       <div>
