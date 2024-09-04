77e91190c5e68749c47b2c0c4f60162f3dbc25ed
acon <101356372+acornitum@users.noreply.github.com>
2024-09-04 16:27:27 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="flex flex-col justify-center">
+        <div className="text-center">
+          <h1>Counterspell</h1>
+        </div>
+        
+      </div>
+      
+
+
     </div>
+
   );
 }
 
