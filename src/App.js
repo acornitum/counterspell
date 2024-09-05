95a45fb911ed7b058e95f1ce516cfe9b1885ff1b
acon <101356372+acornitum@users.noreply.github.com>
2024-09-05 17:26:11 -0400

    change site subplace fonts to inter

--- src/App.js
+++ src/App.js
@@ -13,23 +13,23 @@ function App() {
       >
         <div className="text-center">
           <p className="text-8xl mb-12 text-green">COUNTERSPELL</p>
-          <p className="text-3xl serif mb-4 mx-4">
+          <p className="text-3xl inter mb-4 mx-4">
             Join 5000+ students in any of 250+ locations to create games
           </p>
 
           <div className="flex items-center justify-center">
             <div className="flex items-center justify-center bg-gray-600 w-72 h-10 rounded-lg">
-              <p className="serif">email placeholder</p>
+              <p className="inter">email placeholder</p>
             </div>
           </div>
-          <p className="serif">November 9-10th, 2024 // in-person, worldwide</p>
+          <p className="inter">November 9-10th, 2024 // in-person, worldwide</p>
         </div>
       </div>
 
       <div className="flex flex-col justify-center text-center my-64">
         <div className="m-5">
           <p className="mb-6 text-4xl">imagine what you'll create</p>
-          <p className="serif text-lg max-w-lg mx-auto">
+          <p className="inter text-lg max-w-lg mx-auto">
             This fall, Hack Club is running its biggest hackathon yet: a game
             jam where over 5k teens across 250 locations will come together for
             12-24 hours to make games together.
@@ -52,7 +52,7 @@ function App() {
       <div class="faq" className="flex flex-col justify-center text-center">
         <div className="m-5">
           <p className="mb-6 text-4xl">frequently asked questions</p>
-          <div className="flex items-center justify-center p-5 serif">
+          <div className="flex items-center justify-center p-5 inter">
             <div className="bg-darkpurp rounded-lg p-5">
               <p>question! question!</p>
               <p>answer! answer!</p>
