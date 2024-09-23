9ad5a030ae792bfe7cdb0d8240403e728c50ce19
acon <101356372+acornitum@users.noreply.github.com>
2024-09-23 12:52:38 -0400

    updated faq wording

--- src/faqData.js
+++ src/faqData.js
@@ -1,21 +1,24 @@
 const faqData = [
     {
-        question: "Am I eligible to participate in Counterspell?",
+        question: "Am I eligible to sign up?",
         answer: `If you're 18 or under, yes, we are so excited to see you! If you're over 18 but
         still in high school, shoot us an email at
         <a href="mailto:counterspell@hackclub.com">counterspell@hackclub.com</a>.`,
     },
     {
         question: "Does participating cost anything?",
-        answer: `Nope! We'll have meals, snacks, and beverages onsite at the hackathon, as well as swag, prizes, and fun mini-events.`,
+        answer: `It's entirely free! We'll have meals, snacks, and beverages onsite at the hackathon, as well as swag, prizes, and fun mini-events.`,
     },
     {
         question: "What has Hack Club done before?",
-        answer: `We have run multiple events of this scale around the world. Each one, however is
-        special in its own way. The summer of '21, we 
+        answer: `We have run multiple events around the world. The summer of '21, we 
         <a href="https://www.youtube.com/watch?v=2BID8_pGuqA&ab_channel=HackClub">chartered a train across America</a>
         and ran the world's longest hackathon on land. Last year, we ran an outdoors do-it-yourself camping 
         adventure in Cabot, Vermont. Check it out <a href="https://www.youtube.com/watch?v=O1s5HqSqKi0&ab_channel=HackClub">here</a>!`,
+    },
+    {
+        question: "I'm not good at coding, can I join?",
+        answer: `This hackathon is for hackers of all skill levels! We'll have workshops and other events so join us and let's learn together. If you'd like to start exploring some introductory projects, check out <a href="https://workshops.hackclub.com/">Hack Club Workshops</a>.`,
     },
     {
         question: "What can I make at Counterspell?",
@@ -26,17 +29,13 @@ const faqData = [
         answer: `Your laptop, charger, and an open mind! If your location is overnight, also bring toiletries, and a sleeping bag.`,
     },
     {
-        question: "I'm not good at coding, can I join?",
-        answer: `This hackathon is for hackers of all skill levels! We'll have workshops and other events so join us and let's learn together. If you'd like to start exploring some introductory projects, check out Hack Club Workshops.`,
-    },
-    {
-        question: "My parents are worried! What should I do?",
+        question: "My parents are worried?",
         answer: `We're here to help! Ask them to reach out to us at
           <a href="mailto:counterspell@hackclub.com">counterspell@hackclub.com</a>
           and we'll make sure to answer all their questions!`,
     },
     {
-        question: "I have more questions, how can I reach out?",
+        question: "How can I reach out for more questions?",
         answer: `Contact us! Reach out at
           <a href="https://hackclub.com/slack/">#counterspell</a>
           on the Hack Club Slack or email us at
